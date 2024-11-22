a3a7b51b53c50414ca4a93d115ce0e417dd04f8c
MigQuan <quannmph45405@nnmph45405fpt.edu.vn>
2024-11-22 08:14:44 +0700

    editbase

--- index.php
+++ index.php
@@ -6,10 +6,12 @@ require_once './commons/function.php'; // Hàm hỗ trợ
 
 // Require toàn bộ file Controllers
 require_once './controllers/HomeController.php';
-// require_once './controllers/ProductController.php';
+
 
 
 // Require toàn bộ file Models
+require_once './models/SanPham.php';
+
 
 // Route
 $act = $_GET['act'] ?? '/';
