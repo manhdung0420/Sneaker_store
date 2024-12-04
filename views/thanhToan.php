4942f73162dfedcdede9be9c7c10ac81551444c2
manhdung0420 <dungdmph49130@gmail.com>
2024-12-04 21:41:20 +0700

    finish

--- views/thanhToan.php
+++ views/thanhToan.php
@@ -6,10 +6,10 @@
 <div class="breadcrumb-area">
     <div class="container">
         <div class="breadcrumb-content">
-            <h2>Shop Related</h2>
+            <h2>Thanh toán</h2>
             <ul>
-                <li><a href="index.html">Home</a></li>
-                <li class="active">Cart</li>
+                <li><a href="index.html">Trang chủ</a></li>
+                <li class="active">Thanh toán</li>
             </ul>
         </div>
     </div>
